f9e4b2712b98858c0223d73ba5709a00acbc65d1
sourceloo <luyuanquan1994@foxmail.com>
2016-12-02 19:30:41 +0800

    add model

--- springboot/demo/src/main/java/com/thu/web/HelloController.java
+++ springboot/demo/src/main/java/com/thu/web/HelloController.java
@@ -2,10 +2,10 @@ package com.thu.web;
 
 import com.thu.domain.Student;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 /**
  * Created by source on 11/26/16.
@@ -27,10 +27,17 @@ public class HelloController {
     }
 
 
-    // 这个没有@ResponseBody 会在默认目录src/main/resources/templates下寻找blog模板
-    @GetMapping("/about/blog")
-    public String blog() {
-        return "about/blog";
+    // 这个没有@ResponseBody 看 return值，会在默认目录src/main/resources/templates下寻找blog模板
+    // 增加model。返回到模板时，model会一起返回。
+
+    @GetMapping("/blogs/{id}")
+    public String blog(@PathVariable String id, Model model) {
+
+        model.addAttribute("title", "博文ID是：" + id);
+        model.addAttribute("createdTime", new Date().toString());
+        model.addAttribute("content", "我的第一篇博文！");
+
+        return "blogs/index";
     }
 
 }
--- springboot/demo/src/main/java/com/thu/web/StudentController.java
+++ springboot/demo/src/main/java/com/thu/web/StudentController.java
@@ -1,19 +1,22 @@
 package com.thu.web;
 
 import com.thu.domain.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by source on 12/1/16.
  */
 
-@RestController
+@Controller
 @RequestMapping(value = "/students") // 响应root/students/xxx路径
 public class StudentController {
 
     /* 返回的对象作为json字符串形式 */
     @GetMapping(value = "/way1/{studentId}") // 响应root/students/way1/2016 其中2016作为参数传入
-    public Student getStudent(@PathVariable Integer studentId)
+    @ResponseBody
+    public Student getStudent(@PathVariable Integer studentId) // 特别地，这些@PathVariable与@RequestParam都可以省略
     {
         System.out.println("前台传入的用户id是：" + studentId);
 
@@ -26,6 +29,7 @@ public class StudentController {
 
     //@GetMapping(value = "") //响应路径为students/way2?studentId=, 注意不要添加/ 下面这种更好
     @GetMapping(value = "/way2")
+    @ResponseBody
     public Student getStudent2(@RequestParam(name = "studentId", required = false, defaultValue = "0") Integer studentId) // 或者(@RequestParam Integer studentId)
     {
         System.out.println("前台传入的用户id是：" + studentId);
@@ -34,4 +38,15 @@ public class StudentController {
         return  new Student("2017", "luyq");
 
     }
+
+    // 像model中添加对象
+    @GetMapping(value = "/{id}")
+    public String show(@PathVariable String id, Model model)
+    {
+        model.addAttribute(new Student(id, "luyq"));
+        model.addAttribute("hobby", "luyq likes swimming!");
+
+        return "students/index";
+    }
+
 }
