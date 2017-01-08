687085495fdcfb280d2755c19817771982d83d9a
dc3671 <chenzhh3671@gmail.com>
2017-01-08 22:23:45 +0800

    feat(school): fix school part route

---
+++ project/src/main/java/com/thu/web/school/PageController.java
@@ -0,0 +1,25 @@
+package com.thu.web.student;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/school")
+public class PageController {
+
+    @GetMapping("/manage_console")
+    public String getMain()
+    {
+        return "school/manage_console";
+    }
+
+    @GetMapping("/manage_console/{foo:.+}")
+    public String getMain2()
+    {
+        return "redirect:/school/manage_console";
+    }
+
+}
