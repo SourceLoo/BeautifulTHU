f81cd76774005a38690b8ce5b51bc6ed005f92aa
Jason <yb-li16@mails.tsinghua.edu.cn>
2017-02-18 18:10:53 +0800

    fix problem about LocalDateTime converting to DB type

--- project/src/main/java/com/thu/Application.java
+++ project/src/main/java/com/thu/Application.java
@@ -2,9 +2,11 @@ package com.thu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 //@SpringBootApplication
@@ -19,6 +21,9 @@ import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 //@Configuration
 //@EnableAutoConfiguration
 //@ComponentScan 这三个和springbootapplication 等价
+@EntityScan(
+        basePackageClasses = { Application.class, Jsr310JpaConverters.class }
+)
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
